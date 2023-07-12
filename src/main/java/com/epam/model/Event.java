d88f977bb7eab9bbe683caaab80818d92fc548ea
ruslan-gorod <ruslan-gorod@ukr.net>
2023-07-13 01:06:08 +0300

    Added Swagger

--- src/main/java/com/epam/model/Event.java
+++ src/main/java/com/epam/model/Event.java
@@ -1,10 +1,17 @@
 package com.epam.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Data
